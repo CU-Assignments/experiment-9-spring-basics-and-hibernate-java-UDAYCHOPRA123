9b0f3e4e8518d50d86e3b477fe36e7918f4cfaf3
UDAY CHOPRA <udayhero02@gmail.com>
2025-04-10 12:38:21 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,37 @@
+public class Course {
+    private String courseName;
+    private int duration;
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Course: " + courseName + ", Duration: " + duration + " weeks";
+    }
+}
+
+public class Student {
+    private String name;
+    private Course course;
+
+    public Student(String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student: " + name);
+        System.out.println(course);
+    }
+}
